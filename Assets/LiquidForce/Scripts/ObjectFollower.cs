29b5839a219851ca7322730fda377a4a98f680b7
Brian A. Lee <octopus8creations@gmail.com>
2026-01-16 20:12:54 -0800

    Working on improving ObjectFollower.

--- Assets/LiquidForce/Scripts/ObjectFollower.cs
+++ Assets/LiquidForce/Scripts/ObjectFollower.cs
@@ -33,6 +33,13 @@ namespace LiquidForce
 
         [SerializeField] private Vector3 maxPositionOffset;
 
+        [SerializeField]
+        float positionSpeed = 1f;
+        
+        [SerializeField]
+        float rotationSpeed = 0.1f;
+
+
         [Header("Update")]
         [SerializeField] private bool updateRotationX = true;
         [SerializeField] private bool updateRotationY = true;
@@ -158,7 +165,6 @@ namespace LiquidForce
                 Vector3 targetPosition = target.position;
                 Vector3 targetRotation = target.rotation.eulerAngles;
 
-                float positionSpeed = 0.1f;
                 if (Mathf.Abs(targetPosition.x - source.position.x) > maxPositionOffset.x)
                 {
                     targetPosition.x = Mathf.Lerp(targetPosition.x, source.position.x, positionSpeed * Time.deltaTime);
@@ -175,7 +181,6 @@ namespace LiquidForce
                 }
 
 
-                float rotationSpeed = 0.1f;
                 if (updateRotationX)
                 {
                     if (Mathf.Abs(targetRotation.x - source.rotation.eulerAngles.x) > maxRotationOffsetDegrees.x)
@@ -203,6 +208,111 @@ namespace LiquidForce
             }
             oneTimeUpdate = false;
         }
-    }
-    
+
+
+        private bool isSettingPositionX;
+        private bool isSettingPositionY;
+        private bool isSettingPositionZ;
+        private bool isSettingRotationX;
+        private bool isSettingRotationY;
+        private bool isSettingRotationZ;
+        
+        private void UpdateTransformNew()
+        {
+            // If no targets have been set, then do nothing.
+            if (targets == null)
+            {
+                return;
+            }
+
+            if (null == source)
+            {
+                source = gameObject.transform;
+            }
+
+            foreach (var target in targets)
+            {
+                if (target == null)
+                {
+                    Debug.Log("ObjectFollower: target is null.");
+                    return;
+                }
+                Vector3 targetPosition = target.position;
+                Vector3 targetRotation = target.rotation.eulerAngles;
+                
+                float positionSetMargin = 0.01f;
+                float rotationSetMargin = 1.0f;
+                float abs = Mathf.Abs(targetPosition.x - source.position.x);
+                if (abs < positionSetMargin)
+                {
+                    isSettingPositionX = false;
+                }
+                if (abs > maxPositionOffset.x || isSettingPositionX)
+                {
+                    targetPosition.x = Mathf.Lerp(targetPosition.x, source.position.x, positionSpeed * Time.deltaTime);
+                }
+                
+                abs = Mathf.Abs(targetPosition.y - source.position.y);
+                if (abs < positionSetMargin)
+                {
+                    isSettingPositionY = false;
+                }
+
+                if (abs > maxPositionOffset.y || isSettingPositionY)
+                {
+                    targetPosition.y = Mathf.Lerp(targetPosition.y, source.position.y, positionSpeed * Time.deltaTime);
+                }
+                
+                abs = Mathf.Abs(targetPosition.z - source.position.z);
+                if (abs < positionSetMargin)
+                {
+                    isSettingPositionZ = false;
+                }
+
+                if (abs > maxPositionOffset.z || isSettingPositionZ)
+                {
+                    targetPosition.z = Mathf.Lerp(targetPosition.z, source.position.z, rotationSpeed * Time.deltaTime);
+                }
+                
+                abs = Mathf.Abs(targetRotation.x - source.rotation.eulerAngles.x);
+                if (abs < rotationSetMargin)
+                {
+                    isSettingRotationX = false;
+                }
+
+                if (abs > maxRotationOffsetDegrees.x || isSettingRotationX)
+                {
+                    targetRotation.x = Mathf.Lerp(targetRotation.x, source.rotation.x, rotationSpeed * Time.deltaTime);
+                }
+                
+                abs = Mathf.Abs(targetRotation.y - source.rotation.y);
+                if (abs < rotationSetMargin)
+                {
+                    isSettingRotationY = false;
+                }
+
+                if (abs > maxRotationOffsetDegrees.y || isSettingRotationY)
+                {
+                    targetRotation.y = Mathf.Lerp(targetRotation.y, source.rotation.y, rotationSpeed * Time.deltaTime);
+                }
+                
+                abs = Mathf.Abs(targetRotation.z - source.rotation.z);
+                if (abs < rotationSetMargin)
+                {
+                    isSettingRotationZ = false;
+                }
+
+                if (abs > maxRotationOffsetDegrees.z || isSettingRotationZ)
+                {
+                    targetRotation.z = Mathf.Lerp(targetRotation.z, source.rotation.z, rotationSpeed * Time.deltaTime);
+                }
+                
+                target.SetPositionAndRotation(targetPosition, Quaternion.Euler(targetRotation));
+            }
+            oneTimeUpdate = false;
+        }
+        
+        
+    }        
 }
+    
