ef98a98cc9aff969f05746131f89b8384d6bd93d
Brian A. Lee <octopus8creations@gmail.com>
2026-01-17 20:01:46 -0800

    Working on UIToolkit demo.

---
+++ Assets/[App]/Dev/UIToolkit/TestOscillator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class TestOscillator : MonoBehaviour
+{
+    private float testval = 0;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        testval = (Mathf.Sin(Time.time) + 1.0f) * 0.5f;
+        Debug.Log(testval);
+    }
+}
