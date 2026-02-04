58ea83f65a38c51ff61bcff51810c6bfb69aae62
Brian A. Lee <octopus8creations@gmail.com>
2026-02-03 21:28:58 -0800

    Imported PolygonSciFiCity. UnitMoverSystem moves unit.

--- Assets/_App/DOTS Scene/ECSSystems/UnitMoverSystem.cs
+++ Assets/_App/DOTS Scene/ECSSystems/UnitMoverSystem.cs
@@ -14,9 +14,16 @@ partial struct UnitMoverSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var localTransform in SystemAPI.Query<RefRW<LocalTransform>>())
+        foreach (var (
+                 localTransform,
+                 moveSpeed
+                 )
+                 in SystemAPI.Query<
+                     RefRW<LocalTransform>,
+                     RefRO<MoveSpeed>
+                 >())
         {
-            localTransform.ValueRW.Position = localTransform.ValueRO.Position + new float3(1, 0, 0) * SystemAPI.Time.DeltaTime;
+            localTransform.ValueRW.Position = localTransform.ValueRO.Position + new float3(0, 0, -1) * SystemAPI.Time.DeltaTime;
         }
     }
 
