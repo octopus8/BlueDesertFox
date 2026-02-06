3b68b2ef5d0b42c9cd6af472b0c0012c8bc40dee
Brian A. Lee <octopus8creations@gmail.com>
2026-02-05 17:37:08 -0800

    Working on directing the zero using the spline.

--- Assets/_App/Test Scenes/PhysicsTest.cs
+++ Assets/_App/Test Scenes/PhysicsTest.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 
 public class PhysicsTest : MonoBehaviour
 {
+    [SerializeField] private GameObject target;
     [SerializeField] private Rigidbody testObject;
 
 
+
+    private bool hasSetInitialVelocity = false;
+
     private void FixedUpdate()
     {
-        testObject.linearVelocity = new Vector3(0, 0, -10);
+        if (!hasSetInitialVelocity)
+        {
+//            testObject.linearVelocity = new Vector3(0, 0, 10);
+            hasSetInitialVelocity = true;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - testObject.transform.position);
+        testObject.transform.rotation = Quaternion.Slerp(testObject.transform.rotation, targetRotation, 0.1f);
     }
 }
