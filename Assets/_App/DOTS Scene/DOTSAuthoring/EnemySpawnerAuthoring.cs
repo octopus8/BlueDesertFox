c1674d9ed59ee0df0efb04ff789b5deb62c08069
Brian A. Lee <octopus8creations@gmail.com>
2026-02-16 18:27:02 -0800

    Commented.

--- Assets/_App/DOTS Scene/DOTSAuthoring/EnemySpawnerAuthoring.cs
+++ Assets/_App/DOTS Scene/DOTSAuthoring/EnemySpawnerAuthoring.cs
@@ -3,9 +3,17 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Splines;
 
+/// <summary>
+/// Authoring component for the EnemySpawner system. It allows designers to specify a SplineContainer and sample count
+/// in the editor, which are then used to create a SplineDataComponent that defines the path for spawning enemies.
+/// The Baker class converts the authoring data into runtime components and blob assets for efficient access during gameplay.
+/// </summary>
 public class EnemySpawnerAuthoring : MonoBehaviour
 {
+    /// <summary> The SplineContainer that defines the path along which enemies will be spawned. This should be assigned in the Unity Editor./// </summary>
     [SerializeField] private SplineContainer splineContainer;
+    
+    /// <summary> The number of sample points to generate along the spline. Higher values will result in smoother movement but may increase memory usage./// </summary>
     [SerializeField] private int sampleCount = 100;
     
     public class Baker : Baker<EnemySpawnerAuthoring>
@@ -20,6 +28,12 @@ public class EnemySpawnerAuthoring : MonoBehaviour
             });
         }
 
+        /// <summary>
+        /// Creates a SplineDataComponent from the given SplineContainer. It samples the spline at a specified number of points and stores the data in a blob asset for efficient access at runtime.
+        /// </summary>
+        /// <param name="splineContainer"></param>
+        /// <param name="sampleCount"></param>
+        /// <returns></returns>
         private SplineDataComponent CreateSplineDataComponent(SplineContainer splineContainer, int sampleCount)
         {
             if (splineContainer is null)
@@ -47,6 +61,10 @@ public class EnemySpawnerAuthoring : MonoBehaviour
     }
 }
 
+
+/// <summary>
+/// Component that holds data for spawning enemies along a spline. It contains a flag to trigger spawning and a reference to the spline data that defines the path for the enemies to follow.
+/// </summary>
 public struct EnemySpawner : IComponentData
 {
     public bool doSpawn;
