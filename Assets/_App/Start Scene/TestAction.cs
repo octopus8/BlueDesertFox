a32f485ead9dc3e6c9993980330fa40f61acf3d5
Brian A. Lee <octopus8creations@gmail.com>
2026-07-28 19:38:47 -0700

    Working on rebuilding the Ace of Ages scene.

--- Assets/_App/Start Scene/TestAction.cs
+++ Assets/_App/Start Scene/TestAction.cs
@@ -35,7 +35,7 @@ public class TestAction : MonoBehaviour
     {
         
         SceneSelectUIState sceneSelectUIState = FindFirstObjectByType<SceneSelectUIState>();
-        sceneSelectUIState.LoadScene(sceneList.scenes[1]);
+        sceneSelectUIState.LoadScene(sceneList.scenes[0]);
     }
     
     
